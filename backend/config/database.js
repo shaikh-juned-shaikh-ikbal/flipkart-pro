8d9d94a261466d48cdb66f6ddf62c0b0445a0726
Shaikh juned <sheikhjuned770@gmail.com>
2023-08-28 13:20:58 +0530

    first commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://sheikhjuned770:y7wT1rQNSOUtPgjR@flipkart-pro.r8p15uo.mongodb.net/?retryWrites=true&w=majorityzzMm33tJ9oM1FeSh";
 
 const connectDatabase = () => {
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
-            console.log("Mongoose Connected");
+            console.log("Mongoose Connected ");
         });
 }
 
